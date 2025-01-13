c567de8f5fb5d7b741b227cf9ff5208ade8309b0
Gabriel Almeida <gabrielsa.tech@gmail.com>
2025-01-13 20:05:37 -0300

    web-api update

--- db.js
+++ db.js
@@ -13,4 +13,27 @@ function selectCustomer(id){
     return customers.find(c => c.id === id);
 }
 
-module.exports = { selectCustomers, selectCustomer };
+function insertCustomer(customer){
+    customers.push(customer);
+}
+
+function updateCustomer(id, customerData){
+    const customer = customers.find(c => c.id === id);
+    if(!customer) return;
+    customer.nome = customerData.nome;
+    customer.idade = customerData.idade;
+    customer.uf = customerData.uf;
+}
+
+function deleteCustomer(id){
+    const index = customers.findIndex(c => c.id === id);
+    customers.splice(index, 1);
+}
+
+module.exports = { 
+        selectCustomers,
+        selectCustomer,
+        insertCustomer,
+        updateCustomer,
+        deleteCustomer    
+    };
--- index.js
+++ index.js
@@ -6,6 +6,27 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.delete("/clientes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    res.json(db.deleteCustomer(id));
+    res.sendStatus(204);
+})
+
+app.put("/clientes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const customer = req.body;
+    db.updateCustomer(id, customer);
+    response.sendStatus(200); 
+})
+
+app.post("/clientes", (req, res) => {
+    const customer = req.body;
+    db.insertCustomer(customer);
+    res.sendStatus(201);    
+})
+
 app.get("/clientes/:id", (req, res) => {
     const id = parseInt(req.params.id);
     res.json(db.selectCustomer(id));
@@ -21,6 +42,6 @@ app.get("/", (req, res, next) => {
     })
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
     console.log("O App está rodando!");
 })
